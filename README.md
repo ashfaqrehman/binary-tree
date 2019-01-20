48304f6d5d925defbb51bc7f1e1c73e51fe301ff
Ashfaq Rehman <rehman@sheridancollege.ca>
2019-01-19 23:11:56 -0500

    updated solution.py updated .gitignore to include .vscode compiled files updated README.md

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # K-th ancestor of a node in Binary Tree
-Given a binary tree in which nodes are numbered from 1 to n. Given a node and a positive integer K. We have to print the K-th ancestor of the given node in the binary tree. If there does not exist any such ancestor then print -1.
 
-For example in the below given binary tree, 2nd ancestor of node 4 and 5 is 1. 3rd ancestor of node 4 will be -1.
-![alt text](http://contribute.geeksforgeeks.org/wp-content/uploads/tree122-1.gif)
+Assume you are given a tree of N nodes. The nodes are numbered from 0 to N−1 and their parents are represented in an array A, such that A[i] denotes the number of the parent of the i-th node. Node 0 is the root node and it will not have a parent, so the corresponding value in array A will be −1. We define the distance between two nodes to be the length of the shortest path between them, and we define an ancestor of the i-th node as any node lying on the shortest path between the i-th node and the root. Your goal is to find the ancestor at distance D of every node of the tree.
+
+function solution(D, A);
+
+that, given an integer D and array A of N integers, returns an array of N integers representing the ancestors at distance D of the consecutive nodes. If a node doesn't have an ancestor at distance D its field should contain −1.
+
+For example, given integer D = 2 and array A such that:
+
+A[0] = -1
+A[1] = 0
+A[2] = 4
+A[3] = 2
+A[4] = 1
+
+your function should return [−1, −1, 0, 1, −1].
+
+the first element of array A equals −1;
+array A represents a valid tree.
--- solution.py
+++ solution.py
@@ -1,14 +1,62 @@
 #!/usr/bin/env python
+'''
+function solution(D, A): Given an integer D and array A of N integers, 
+   returns an array of N integers representing the ancestors at distance D of the consecutive nodes. If a node doesn't have an ancestor at distance D its field should contain −1
+For example, given integer D = 2 and array A such that:
 
-def array_to_dict(A):    
+A[0] = -1
+A[1] = 0
+A[2] = 4
+A[3] = 2
+A[4] = 1
+
+your function should return [−1, −1, 0, 1, −1].
+'''
+
+# function list_to_dict(A): Given an list of N integers
+#   returns a dictionary of N integers
+def list_to_dict(A):    
     return dict(enumerate(A))    
+
+# function at_root(element): Given an integer
+#   returns True if integer is 0 or 1 ie
+def at_root(element):
+    if element==0 or element==-1:
+        return True
+
+    return False
+
+
+def solution(D, A):
+    adict = list_to_dict(A)
+    array = []
+    for element in A:
+        print("Node..",element)        
+        if at_root(element):
+            print("Already at root skipping..Outside loop")
+            array.append(-1)
+            print(array)
+            continue
+        current_node=element        
+        for itr in range(D):            
+            ancestor=adict[current_node]                       
+            print("D=",itr,"A[",current_node,"]->",ancestor)
+            if at_root(ancestor):
+                print("Already at root skipping..Inside loop")
+                #array.append(-1)
+                array.append(-1)
+                continue            
+            current_node=ancestor
+        array.append(ancestor)
+        print(array)
     
+        
 
-A=[-1, 0, 4, 2, 1]
-print(array_to_dict(A))
+def main():
+    #anum = int(input("Please enter a number"))
+    D=2 # integer D
+    A=[-1, 0, 4, 2, 1] # array A of N integers
+    solution(D,A)
 
-
-
-
-
-
+if __name__ == "__main__":
+    main()
