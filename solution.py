d27f6125dc48afd77b2dc0bc8eac4986c29af912
Ashfaq Rehman <rehman@sheridancollege.ca>
2019-01-21 10:56:35 -0500

    update solution.py

--- solution.py
+++ solution.py
@@ -20,35 +20,31 @@ def list_to_dict(A):
 
 # function at_root(element): Given an integer
 #   returns True if integer is 0 or 1 ie
-def at_root(element):
+def isRoot(element):
     if element==0 or element==-1:
         return True
-
     return False
 
-
+# function solution(D, A): Given an integer D and array A of N integers, 
+#   returns an array of N integers representing the ancestors at distance D of the consecutive nodes. If a node doesn't have an ancestor at distance D its field should contain −1
 def solution(D, A):
     adict = list_to_dict(A)
     array = []
     for element in A:
-        print("Node..",element)        
-        if at_root(element):
-            print("Already at root skipping..Outside loop")
-            array.append(-1)
-            print(array)
-            continue
-        current_node=element       
-        for itr in range(D):            
-            ancestor=adict[current_node]                       
-            print("D=",itr,"A[",current_node,"]->",ancestor)
-            if at_root(ancestor):
-                print("Already at root skipping..Inside loop")
-                ancestor=-1
-                #array.append(-1)
-                break            
+        print("Node..",element)
+        current_node = element
+        distance = 0        
+        while distance < D:            
+            if isRoot(current_node):
+                print("No ancestor ...")
+                ancestor = -1
+                print("D=",distance,"A[",current_node,"]->",ancestor)
+                break
             else:
+                distance += 1
+                ancestor=adict[current_node]
                 current_node=ancestor
-            print("D=",itr,"A[",current_node,"]->",ancestor)
+                print("D=",distance,"A[",current_node,"]->",ancestor)
         print("Adding",ancestor)
         array.append(ancestor)
         print(array)
@@ -56,9 +52,12 @@ def solution(D, A):
         
 
 def main():
-    #anum = int(input("Please enter a number"))
+    #D = int(input("integer D"))
     D=2 # integer D
-    A=[-1, 0, 4, 2, 1] # array A of N integers
+    A=[-1, 0, 1, 2, 3] # array A of N integers
+    for i in range(len(A)):
+        print("A[",i,"]->",A[i])
+
     solution(D,A)
 
 if __name__ == "__main__":
